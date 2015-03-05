5d424692245e0a23766a905f0b862d82ec343fc0
acreilly <acreilly3@gmail.com>
2015-03-05 13:59:37 -0500

    added if statement in case there is no custom price

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -16,26 +16,28 @@ Spree::OrdersController.class_eval do
 
 
   def check_cyo_price
-    product = Spree::Variant.find(params.try(:[], :variant_id)).product
-    custom_price = BigDecimal.new(params[:cyo_price_field].split(" ")[1])
+    unless params[:cyo_price_field].nil?
+      product = Spree::Variant.find(params.try(:[], :variant_id)).product
+      custom_price = BigDecimal.new(params[:cyo_price_field].split(" ")[1])
 
-    if product.cyo_price && product.price <= custom_price
-      variant = Spree::Variant.find(params[:variant_id])
+      if product.cyo_price && product.price <= custom_price
+        variant = Spree::Variant.find(params[:variant_id])
 
-      if product.has_variant_with_price(custom_price) && product.has_variant_with_options(variant.option_values)
+        if product.has_variant_with_price(custom_price) && product.has_variant_with_options(variant.option_values)
 
-        product.variants.map{|v| [v.price, v.id, v.option_values]}.each do |v|
+          product.variants.map{|v| [v.price, v.id, v.option_values]}.each do |v|
             return params[:variant_id] = Spree::Variant.find(v[1]).id if v[0] == custom_price && v[2] == variant.option_values
-        end
+          end
           false
-      else
-        new_variant = Spree::Variant.create(product: product, price: custom_price, option_values: variant.option_values, images: variant.images, hidden: true)
+        else
+          new_variant = Spree::Variant.create(product: product, price: custom_price, option_values: variant.option_values, images: variant.images, hidden: true)
 
-        params[:variant_id] = new_variant.id
+          params[:variant_id] = new_variant.id
+        end
+      else
+        flash[:error] = "Minimum price is #{product.display_price.to_s}."
+        redirect_back_or_default(spree.root_path)
       end
-    else
-      flash[:error] = "Minimum price is #{product.display_price.to_s}."
-      redirect_back_or_default(spree.root_path)
     end
   end
 end
